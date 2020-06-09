9a5fdc6460b395e24e2e1b5ea3c0a9cd061e5172
Pablo Aguirre Hormann <pjaguirr@uc.cl>
2020-06-09 17:43:17 -0400

    9/6

--- Reporte Automático/ImprimirReportes.R
+++ Reporte Automático/ImprimirReportes.R
@@ -1,18 +1,14 @@
-library(purrr)
-
-# Definir función para generación de reportes
-imprimir_reporte <- function(x){
-  rmarkdown::render("ReportePorRegion.Rmd",
-                  params = list(reg = x),
-                  output_file = paste0("Reportes/Reporte_", x, "_", format(Sys.Date(), "%d-%m-%Y"))
-                  )
-}
-
 # Valores que se usarán como "parámetros" (variables) del reporte
 regiones <- c("Tarapacá", "Antofagasta", "Atacama", "Coquimbo", "O'Higgins", 
               "Valparaíso", "Maule", "Biobío", "Araucanía", "Lagos", "Aysén", 
               "Magallanes", "Metropolitana", "Ríos", "Arica", "Ñuble")
 
-# Aplicar la función a cada valor de "regiones"
-walk(regiones, imprimir_reporte)
+# Definir función para generación de reportes
+for (i in regiones){
+  rmarkdown::render("ReportePorRegion.Rmd",
+                  params = list(reg = i),
+                  output_file = paste0("Reportes/Reporte_", i, "_", format(Sys.Date(), "%d-%m-%Y"))
+                  )
+}
+
 
